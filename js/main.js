a68065a5c995401a4548227550d648b787cd1d03
sweety <sweety469682@gmail.com>
2021-01-05 01:11:36 +0600

    full proj up

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var menuList = document.getElementById('menuList');
+
+menuList.style.maxHeight = "0px";
+
+function toggleMenu(){
+    if(menuList.style.maxHeight == "0px"){
+        menuList.style.maxHeight = "160px";
+    }else menuList.style.maxHeight = "0px";
+}
